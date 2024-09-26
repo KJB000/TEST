847660f4867bd24d54d8a09be3e6b3fbc08522b1
KJB000 <smcha524@naver.com>
2024-09-26 17:29:16 +0900

    2nd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-cente">
-      <h1 className="text-10x1 text-blue-800">Welcome Home</h1>
+      <h1 className="text-3x1 text-blue-800 mt-4">Welcome Home</h1>
     </div>
   )
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function postspage() {
   const title = '애국가'
-  const content = '동해물과 백두산이 마르고 닳도록 강의가 안 끝나냐'
+  const content = '동해물과 백두산이 마르고 닳도록'
   return (
     <div>
       <h1 className="text-3x1 font-bold text-red-500">Post page</h1>
