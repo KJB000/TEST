a8dc15fa8c2f3ac3240ee586d913549e858b2a08
KJB000 <smcha524@naver.com>
2024-09-26 17:08:32 +0900

    2nd

---
+++ app/posts/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return (
+    <div>
+      <h1>New Post Page</h1>
+    </div>
+  )
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function postspage() {
+  const title = '애국가'
+  const content = '동해물과 백두산이 마르고 닳도록 강의가 안 끝나냐'
+  return (
+    <div>
+      <h1 className="text-3x1 font-bold text-red-500">Post page</h1>
+      <p>Title : {title}</p>
+      <p>Content : {content}</p>
+    </div>
+  )
+}
