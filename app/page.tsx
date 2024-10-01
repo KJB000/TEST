e07bb2657691b4af06b08d639f2fbec5831062a7
KJB000 <smcha524@naver.com>
2024-10-01 17:05:06 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-cente">
-      <h1 className="text-3x1 text-blue-800 mt-4">Welcome Home</h1>
+    <div className="text-center">
+      <h1 className="text-3x1 text-blue-800">Welcome Home</h1>
     </div>
   )
 }
